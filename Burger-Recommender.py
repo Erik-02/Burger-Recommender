2d1e114e4da823c5465553c563e2985d430dea92
Erik-02 <95616538+Erik-02@users.noreply.github.com>
2023-04-03 14:25:34 +0200

    Update Burger-Recommender.py

--- Burger-Recommender.py
+++ Burger-Recommender.py
@@ -247,8 +247,8 @@ def recommendation():
     # Names of recommended burgers
     burger_names = user_prediction_burgers.index.tolist()
     # Get link to recipe and image of burger
-    bl = user_prediction_burgers['link'].values.ravel().tolist()
-    pl = user_prediction_burgers['photo_link'].values.ravel().tolist()
+    bl = burgers_df['link'].values.ravel().tolist()
+    pl = burgers_df['photo_link'].values.ravel().tolist()
 
 
 
